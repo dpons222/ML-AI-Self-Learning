d95d134bf99bc1092c5f22e4afa0ed007f5c9717
dpons222 <132171788+dpons222@users.noreply.github.com>
2024-04-21 14:16:15 -0400

    Update class_dict.py

--- Classes/class_dict.py
+++ Classes/class_dict.py
@@ -11,14 +11,13 @@ contacts = {}
 contacts["Victor"] = {"phone": "7874221994", "email": "vcortes@gmail.com"}
 contacts["Cara"] = {"phone": "123456789", "email": "caras@email.com"}
 
-# Accessing contact information by name
+
 def get_contact_info(name):
     if name in contacts:
         return contacts[name]
     else:
         return None
 
-# Example usage:
 contact_name = input("enter name here ")
 if get_contact_info(contact_name):
     print(f"Contact information for {contact_name}:")
