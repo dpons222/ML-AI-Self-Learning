abebe051ab4c2484ff45bcc463a8d668ac9d8822
dpons222 <dpons222@gmail.com>
2024-06-13 01:17:44 -0400

    completed the email sender script

--- Python/FIle_IO/email_sender.py
+++ Python/FIle_IO/email_sender.py
@@ -13,7 +13,6 @@ email_counts = {}
 
 #get lines
 for line in handle :
-    print(line)
     if not line.startswith("From ") :
         continue
     else :
@@ -29,10 +28,22 @@ for line in handle :
                 email_counts[email] += 1
                 
  #find highest email count in email_counts dictionary
-highest_emailer = None
+highest_emailer = None #use None instead of 0 because we do not know email count yet 
+                        #and this wiil be an immutable object, tuple
 
-for email, count in email_counts.items():
-    if highest_emailer is None or count > highest_emailer[1]:
+for email, count in email_counts.items(): #iterates through key vaule pairs in dictionary
+    if highest_emailer is None or count > highest_emailer[1]: 
         highest_emailer = (email, count)
 
 print(highest_emailer[0],highest_emailer[1])
+
+
+
+'''
+For Loop Explanation - ^^^^
+This version - "if count > highest_emailer[1]:-" skips the check for highest_emailer is None.
+During the first iteration, 
+when highest_emailer is None, the condition count > highest_emailer[1] causes an error 
+because you are trying to access highest_emailer[1], 
+which does not exist when highest_emailer is None.
+'''
