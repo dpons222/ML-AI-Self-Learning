da35751bd09b43d975252024e2550949cb5f2483
dpons222 <dpons222@gmail.com>
2024-06-16 05:14:40 -0400

    soup

---
+++ Python/Sockets_in_Python/beautiful_soup.py
@@ -0,0 +1,20 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+total_sum = 0
+tags = soup('span')
+
+for tag in tags:
+    content = tag.text.strip()
+    nums = int(content)
+    total_sum += nums
+print("total sum is",total_sum)
