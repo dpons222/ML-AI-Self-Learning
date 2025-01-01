5ebc91ca857d39fe1d25fc1d213d99c43abf483a
dpons222 <dpons222@gmail.com>
2025-01-01 15:04:50 -0400

    more ML notes, Logistic Regression and vocab

--- all_practice.py
+++ all_practice.py
@@ -1,11 +1,24 @@
 import numpy as np
 import pandas as pd
 import scipy.stats as stats
+import matplotlib.pyplot as plt
 
-results = 0
-for i in range(0, 5):
-    results += ((-1)**i) / (1+i)
-    print(f'iteration {i}: {results}')
-print(f'total results: {results}')  
+from sklearn.linear_model import LinearRegression
 
-print(47/60)
+# Sample data
+X = [1], [2], [3], [4], [5]  # Predictor variable
+y = [1,5,10,15,20]            # Response variable
+
+# Create and fit the model
+model = LinearRegression()
+model.fit(X, y)
+
+# Get the coefficient and intercept
+coef = model.coef_
+intercept = model.intercept_
+
+print(f"Coefficient: {coef}")
+print(f"Intercept: {intercept}")
+
+plt.scatter(model.predict(X), y)
+plt.show()
